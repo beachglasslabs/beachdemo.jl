c48d3ee50f96e4b96d67291ea6227e40f77c11b6
Ed Yu <ed@beachglasslabs.org>
2023-05-01 15:53:38 -0700

    Add auth page

--- src/api.jl
+++ src/api.jl
@@ -9,6 +9,17 @@ using Dates
     return html(tmp(init))
 end
 
+@get "/auth" function(req::HTTP.Request)
+    tmp = Template("./templates/auth.html")
+    return html(tmp())
+end
+
+@post "/login" function(req::HTTP.Request)
+end
+
+@post "/register" function(req::HTTP.Request)
+end
+
 staticfiles("../public", "/")
 
 serve()
