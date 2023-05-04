f97ab3926558aebcd5fdf76439f37cbe528b8aab
Ed Yu <ed@beachglasslabs.org>
2023-05-04 16:03:54 -0700

    Use port 3000 for dev server

--- src/BeachglassApi.jl
+++ src/BeachglassApi.jl
@@ -4,23 +4,40 @@ using HTTP
 using Oxygen
 using OteraEngine
 using Dates
+using Umbrella
+
+function readenv(env=".env")
+    open(env, "r") do f
+        while !eof(f)
+            line = strip(readline(f))
+            if isempty(line)
+                continue
+            end
+            name, value = split(line, "=") 
+            if isempty(name) || isempty(value)
+                continue
+            end
+            ENV[strip(name)] = strip(value)
+        end
+    end
+end
 
 @get "/" function(req::HTTP.Request)
-    tmp = Template("./templates/index.html")
+    tmp = Template("./src/templates/index.html")
     init = Dict("time" => now())
-    return html(tmp(jl_init=init))
+    return html(tmp(init))
 end
 
 @get "/auth" function(req::HTTP.Request)
-    tmp = Template("./templates/auth.html")
+    tmp = Template("./src/templates/auth.html")
     return html(tmp())
 end
 
 @get "/profile" function(req::HTTP.Request)
     image = rand(String["/img/default-blue.png", "/img/default-red.png", "/img/default-slate.png", "/img/default-green.png"])
-    tmp = Template("./templates/profile.html")
+    tmp = Template("./src/templates/profile.html")
     init = Dict("name" => "test user", "img" => image)
-    return html(tmp(jl_init=init))
+    return html(tmp(init))
 end
 
 @post "/login" function(req::HTTP.Request)
@@ -29,8 +46,9 @@ end
 @post "/register" function(req::HTTP.Request)
 end
 
-staticfiles("../public", "/")
+readenv()
+staticfiles("public", "/")
 
-serve()
+serve(port=3000)
 
 end # module BeachglassApi
