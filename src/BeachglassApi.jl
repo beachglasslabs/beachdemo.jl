e0cdb53920d4fadc6f985e1a7f9a6a99c324f530
Ed Yu <ed@beachglasslabs.org>
2023-05-04 14:02:05 -0700

    Add profile page

--- src/BeachglassApi.jl
+++ src/BeachglassApi.jl
@@ -8,12 +8,19 @@ using Dates
 @get "/" function(req::HTTP.Request)
     tmp = Template("./templates/index.html")
     init = Dict("time" => now())
-    return html(tmp(init))
+    return html(tmp(jl_init=init))
 end
 
 @get "/auth" function(req::HTTP.Request)
     tmp = Template("./templates/auth.html")
     return html(tmp())
+end
+
+@get "/profile" function(req::HTTP.Request)
+    image = rand(String["/img/default-blue.png", "/img/default-red.png", "/img/default-slate.png", "/img/default-green.png"])
+    tmp = Template("./templates/profile.html")
+    init = Dict("name" => "test user", "img" => image)
+    return html(tmp(jl_init=init))
 end
 
 @post "/login" function(req::HTTP.Request)
